5a6cc6c00d3642a649171436a5e24f02dc8eff56
ajrossetti <Alex@alexanders-mbp.home>
2018-11-27 13:51:58 +0000

    First commit

---
+++ index.js
@@ -0,0 +1,54 @@
+var express = require("express");
+var socket = require("socket.io");
+
+// express app setup
+var app = express();
+var server = app.listen(4000, function(){
+    console.log("Connected");
+});
+
+app.use(express.static(__dirname + '/public'));
+
+var users = [];
+var connections = [];
+
+// sockets
+var io = socket(server);
+
+io.on('connection', function(socket){
+    connections.push(socket);
+    console.log("Connected: %s socket(s) connected", connections.length);
+    console.log("Made socket connection", socket.id);
+
+
+    socket.on('new message', function(data){
+        io.sockets.emit("new message", {msg: data, user: socket.username});
+    });
+
+
+    socket.on('typing', function(data){
+        socket.broadcast.emit('typing', {msg: data, user: socket.username});
+    });
+
+     // Disconnect
+     socket.on("disconnect", function(data){
+        users.splice(users.indexOf(socket.username), 1);
+        updateUsernames();
+        connections.splice(connections.indexOf(socket), 1);
+        console.log("Disconnected: %s socket(s) connected", connections.length);
+    });
+
+
+    // new user
+    socket.on("new user", function(data, callback){
+        callback(true);
+        socket.username = data;
+        users.push(socket.username);
+        updateUsernames();
+    });
+
+    function updateUsernames(){
+        io.sockets.emit('get users', users);
+    }
+
+});
---
+++ public/chat.js
@@ -0,0 +1,70 @@
+// make connection
+var socket = io.connect("http://localhost:4000");
+
+// message from client to server
+var $username = $("#username");
+var $messageForm = $("#messageForm");
+var $handle = $("#handle");
+var $message = $("#message");
+var $button = $("#send");
+var $output = $("#output");
+var $feedback = $("#feedback");
+var $chatWindow = $("#chat-window");
+var $userArea = $("#user-area");
+var $chatArea = $("#chat-area");
+var $userForm = $("#userForm");
+var $users = $("#users");
+
+// when 'send' is clicked emit message
+$messageForm.submit(function(e){
+    e.preventDefault();
+    socket.emit("new message", {
+        message: $message.val(),
+        handle: $handle.val()
+    });
+    $message.val("");
+
+    $chatWindow.animate({
+        scrollTop: $chatWindow.get(0).scrollHeight
+    }, 1000);
+});
+
+$userForm.submit(function(e){
+    e.preventDefault();
+    socket.emit("new user", $username.val(), function(data){
+        if (data){
+            $userArea.hide();
+            $chatArea.css("display", "flex");
+        }
+    });
+    $username.val("");
+});
+
+$message.keypress(function(){
+    socket.emit("typing", {
+        message: $message.val(),
+        handle: $handle.val()
+    });
+});
+
+socket.on('new message', function(data){
+    $feedback.html("");
+    
+    // get time;
+    var now = new Date();
+    var time = "("+now.getHours()+":"+("0"+now.getMinutes()).slice(-2)+")";
+
+    $output.append("<p><em>"+ time +"</em><strong> "+ data.user +"</strong>: " + data.msg.message +"</div>");
+});
+
+socket.on('typing', function(data){
+    $feedback.html("<p><em>"+data.user+" is typing a message...</em></p>");
+});
+
+socket.on('get users', function(data){
+    var html = '';
+    for (i = 0; i < data.length; i++){
+        html += "<li class='list-group-item'>"+ data[i] +"</li>";
+    }
+    $users.html(html);
+}); 
